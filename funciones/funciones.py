140cef8eb6a72b61042b39811b3976505a169f6b
Juan-A-Mendoza <juan.a.m.b.9@gmail.com>
2026-05-02 11:33:09 -0400

    calculadora

---
+++ funciones/funciones.py
@@ -0,0 +1,8 @@
+print ("Funciones simples con parametros")
+
+def saludar(nombre):
+    #retorna un saludo simple.
+    return f"Hola, {nombre}"
+
+if __name__ == "__main__":
+    print(saludar("Mundo"))
