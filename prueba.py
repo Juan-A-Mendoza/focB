1f02d0cd8af76cbf9e1f83389de69a1cfcc9cd53
Juan-A-Mendoza <juan.a.m.b.9@gmail.com>
2026-05-02 09:36:45 -0400

    actualizacion archivo py

--- prueba.py
+++ prueba.py
@@ -1,1 +1,8 @@
-print("Hola")
+#print("Hola Mundo")
+
+num1 = int(input("Ingrese el primer numero:"))
+num2 = int(input("Ingresa el segundo numero:"))
+
+resultado = num1 + num2
+
+print ("El resultado es:", resultado)
