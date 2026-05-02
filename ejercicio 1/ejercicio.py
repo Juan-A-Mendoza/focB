f78891354bb3082b404ad9e674cede5d9f745cd0
Juan-A-Mendoza <juan.a.m.b.9@gmail.com>
2026-05-02 10:22:47 -0400

    ejercicio 1 agregado

---
+++ ejercicio 1/ejercicio.py
@@ -0,0 +1,23 @@
+print("""Condicionales y bucles basicos""")
+x = 3 
+if x > 0:
+    print ("positivo")
+elif x == 0:
+    print("cero")
+else:
+    print("negativo")
+
+
+print("Estructura for")    
+for i in range(3):
+    print(f"i={i}")
+    print("hola")
+    
+    
+print ("estructura while")
+n = 3
+while n > 0:
+    print(n)
+    n-= 1
+    
+    
